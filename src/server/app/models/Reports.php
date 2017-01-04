bed9eebdb10b6ca61022c3dae0679ccfa38e666d
Yuki Kitamura <match5121@gmail.com>
2017-01-04 13:07:27 +0900

    parseメソッドを削除 ReportsRepositoryをテストの通るように実装

--- src/server/app/models/Reports.php
+++ src/server/app/models/Reports.php
@@ -1,28 +1,14 @@
 <?php
 
-class Comment {
+class Comment extends Model
+{
     public $user_id;
-    public $comment;
-    public $posted;
-
-    static function parse($data) {
-        $data = (object) $data;
-        $comment = new Comment();
-
-        if (isset($data->user_id)) {
-            $comment->user_id = $data->user_id;
-        }
-        if (isset($data->comment)) {
-            $comment->comment = $data->comment;
-        }
-        if (isset($data->posted)) {
-            $comment->posted = $data->posted;
-        }
-        return $comment;
-    }
+    public $message;
+    public $post_date_time;
 }
 
-class Report {
+class Report extends Model
+{
     public $year;
     public $weeknum;
     public $user_id;
@@ -31,65 +17,127 @@ class Report {
     public $publish_date_time;
     public $publish_comment;
 
-    function addData($key, $value) {
+    function addData($key, $value) 
+    {
         $this->data[$key] = $value;
     }
 
-    function addComments($user_id, $message) {
+    function addComment($user_id, $message) 
+    {
         $comment = new Comment();
-        $comment->user_id = $userId;
-        $comment->comment = $message;
-        $comment->posted = date('Y-m-d H:i:s');
+        $comment->user_id = $user_id;
+        $comment->message = $message;
+        $comment->post_date_time = $this->stampDateTime();
         $this->comments[] = $comment;
     }
 
-    function publish($publish_comment) {
-        $this->publish_date_time = date('Y-m-d H:i:s');
-        $this->publish_comment = $publish_comment;
+    function stampDateTime()
+    {
+        return new Datetime();
     }
 
-    static function parse($data) {
-        $data = (object) $data;
-        $report = new Report();
-
-        if (isset($data->year)) {
-            $report->year = $data->year;
-        }
-        if (isset($data->weeknum)) {
-            $report->weeknum = $data->weeknum;
-        }
-        if (isset($data->user_id)) {
-            $report->user_id = $data->user_id;
-        }
-        if (isset($data->data)) {
-            $report->data = $data->data;
-        }
-        if (isset($data->comments)) {
-            foreach($data->comments as $comment) {
-                $report->comments[] = Comment::parse($comment);
-            }
-        } else {
-            $report->comments = [];
-        }
-        return $report;
+    function publish($publish_comment)
+    {
+        $this->publish_date_time = $this->stampDateTime();
+        $this->publish_comment = $publish_comment;
     }
 }
 
-class ReportsRepository {
-    
+class ReportsRepository
+{
+
     private $db;
-    
-    function __construct($db) {
+
+    function __construct($db) 
+    {
         $this->db = $db;
     }
-    
-    function save($report) {
-    }
-    
-    function reportsForWeek($year, $weekNum) {
+
+    public function findById($year, $weeknum, $user_id)
+    {
+        $items = $this->findItemsById($year, $weeknum, $user_id);
+        if ($items) {
+            $aReport = new Report();
+            $aReport->year    = $year;
+            $aReport->weeknum = $weeknum;
+            $aReport->user_id = $user_id;
+            foreach($items as $aItem)
+                $aReport->addData($aItem->item_name, $aItem->content);
+        } else {
+            return null;
+        }
+
+        $aPublication = $this->findPublicationById($year, $weeknum, $user_id);
+        if ($aPublication) {
+            $aReport->publish_comment   = $aPublication->publish_comment;
+            $aReport->publish_date_time = new Datetime($aPublication->publish_date_time);
+        }
+
+        $comments = $this->findCommentsById($year, $weeknum, $user_id);
+        foreach($comments as $source) {
+            $aComment = new Comment();
+            $aComment->message = $source->message;
+            $aComment->post_date_time = new Datetime($source->post_date_time);
+            $aComment->user_id = $source->sender_user_id;
+            $aReport->comments[] = $aComment;
+        }
+
+        return $aReport;
     }
 
-    function latestReportsByUser($userId, $key, $fetchCount, $page) {
+    function findItemById($year, $weeknum, $user_id, $item_name)
+    {
+        return $this->db->getSingleResult("SELECT * FROM weekly_report_items WHERE year = ? AND weeknum = ? AND user_id = ? AND item_name = ?"
+                                         , $year, $weeknum, $user_id, $item_name);
+    }
+
+    private function findItemsById($year, $weeknum, $user_id)
+    {
+        return $this->db->getArrayResult('SELECT * FROM weekly_report_items '
+                                          . 'WHERE year    = ? '
+                                          . 'AND   weeknum = ? '
+                                          . 'AND   user_id = ? '
+                                         , $year, $weeknum, $user_id);
+    }
+
+    private function findPublicationById($year, $weeknum, $user_id)
+    {
+        return $this->db->getSingleResult('SELECT * FROM weekly_reports '
+                                          . 'WHERE year    = ? '
+                                          . 'AND   weeknum = ? '
+                                          . 'AND   user_id = ? '
+                                         , $year, $weeknum, $user_id);
+    }
+
+    private function findCommentsById($year, $weeknum, $user_id)
+    {
+        return $this->db->getArrayResult('SELECT * FROM weekly_report_comments '
+                                         . 'WHERE year    = ? '
+                                         . 'AND   weeknum = ? '
+                                         . 'AND   user_id = ? '
+                                         . 'ORDER BY "index" ASC '
+                                        , $year, $weeknum, $user_id);
+    }
+
+    function save($report)
+    {
+        $result = $this->db->executeQuery("INSERT INTO weekly_reports (year, weeknum, user_id, publish_date_time, publish_comment) values (?, ?, ?, ?, ?)"
+                                         , $report->year, $report->weeknum, $report->user_id, $report->publish_date_time->format('Y-m-d H:i:s'), $report->publish_comment);
+        return $result;
+    }
+
+    private function publish($report) 
+    {
+        $result = $this->db->executeQuery("INSERT INTO weekly_reports (year, weeknum, user_id, publish_date_time, publish_comment) values (?, ?, ?, ?, ?)"
+                                         , $report->year, $report->week, $report->user_id, $report->publish_date_time, $report->publish_comment);
+    }
+
+    function reportsForWeek($year, $weekNum) 
+    {
+    }
+
+    function latestReportsByUser($userId, $key, $fetchCount, $page) 
+    {
     }
 
 }
