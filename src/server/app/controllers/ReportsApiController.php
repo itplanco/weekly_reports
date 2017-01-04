368712f058baf58b8f85459d5a3db3be71118f2f
Yuki Kitamura <match5121@gmail.com>
2017-01-04 13:18:28 +0900

    ブロックの記述スタイルを変更

--- src/server/app/controllers/ReportsApiController.php
+++ src/server/app/controllers/ReportsApiController.php
@@ -1,9 +1,11 @@
 <?php
 
-class ReportsApiController extends Controller {
+class ReportsApiController extends Controller
+{
     private $repository;
-    
-    function __construct($repository = NULL) {
+
+    function __construct($repository = NULL)
+    {
         if ($repository) {
             $this->repository = $repository;
         } else {
@@ -15,9 +17,10 @@ class ReportsApiController extends Controller {
      * 特定のレポートを取得する
      * /api/reports/:year/:weeknum/:user_id
      */
-    function get($year, $weeknum, $user_id) {
+    function get($year, $weeknum, $user_id)
+    {
         $report = $this->repository->findById($year, $weeknum, $user_id);
-        
+
         if (!$report) {
             return $this->notFound();
         }
@@ -29,9 +32,10 @@ class ReportsApiController extends Controller {
      * 特定のレポートを更新する
      * /api/reports/:year/:weeknum/:user_id
      */
-    function put($year, $weeknum, $user_id, $data) {
+    function put($year, $weeknum, $user_id, $data)
+    {
         $report = $this->repository->findById($year, $weeknum, $user_id);
-        
+
         if (!$report) {
             return $this->notFound();
         }
